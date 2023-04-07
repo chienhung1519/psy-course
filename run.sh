b4852c66b1d382238028b021590696d456461324
chienhung1519 <chienhung1519@gmail.com>
2023-04-07 16:45:50 +0800

    debug: clear out time expression when TimeInfo is 0

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ python run.py \
   --train_file data/processed/data_230406_train.xlsx \
   --eval_file data/processed/data_230406_eval.xlsx \
   --test_file data/processed/data_230406_test.xlsx \
-  --output_dir ./outputs/clinical-t5/ \
+  --output_dir ./outputs/flan-t5-base/ \
   --model_name_or_path google/flan-t5-base

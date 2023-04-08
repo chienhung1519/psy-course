c72c6840002d44142e20b2073a65575047e6398a
chienhung1519 <chienhung1519@gmail.com>
2023-04-08 12:00:52 +0800

    Run t5-base

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ python run.py \
   --train_file data/processed/data_230406_train.xlsx \
   --eval_file data/processed/data_230406_eval.xlsx \
   --test_file data/processed/data_230406_test.xlsx \
-  --output_dir ./outputs/flan-t5-base/ \
-  --model_name_or_path google/flan-t5-base
+  --output_dir ./outputs/t5-base/ \
+  --model_name_or_path t5-base
